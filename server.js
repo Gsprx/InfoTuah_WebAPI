a1f8b79bce3e880907ba4d81fd81cb37c221b0ae
Gaspar <rigos.gasparis@gmail.com>
2024-12-22 23:57:36 +0200

    project structure change for better clarity and easier nodejs app execution

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 // Node.js Server script
 const uuid = require("uuid");
+const path = require('path');
 const express = require("express");
+
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html for the root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = 8080;
+let server = app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
