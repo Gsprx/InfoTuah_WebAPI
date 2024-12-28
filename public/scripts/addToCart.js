648abb64b2ce91addcd07cb1ed07d67b44d651ba
Gaspar <rigos.gasparis@gmail.com>
2024-12-28 17:24:40 +0200

    added error box showing when adding items to cart without login

--- public/scripts/addToCart.js
+++ public/scripts/addToCart.js
@@ -14,13 +14,38 @@ const observer = new MutationObserver(() => {
     const buttons = document.querySelectorAll(".add-to-cart-btn");
     buttons.forEach((btn) => {
       btn.addEventListener("click", () => {
-        //send http request to server for adding item to cart
+        //check if user is logged in
+        if (true || !userLoggedIn()) {
+          //show error message when trying to add cart without log in
+          const closeError = document.querySelector(".close-error-button");
+          closeError.addEventListener("click", () => {
+            hideError();
+          });
+          showError("User must be logged in to add items to cart.");
+          return;
+        }
+
         const itemId = btn.getAttribute("data-id");
+        //send http request to server for adding item to cart
         console.log(`Item with id: ${itemId} added to the cart.`);
       });
     });
   }, 100); // 100ms delay
 });
+
+// show generic error box with given text as error message
+function showError(errorText) {
+  const errorBox = document.querySelector(".error-container");
+  const errorMessage = document.querySelector(".error-message");
+  errorMessage.textContent = errorText;
+  errorBox.style.display = "block";
+}
+
+//hide error box
+function hideError() {
+  const errorBox = document.querySelector(".error-container");
+  errorBox.style.display = "none";
+}
 
 // observe changes to the document body
 observer.observe(document.body, { childList: true, subtree: true });
