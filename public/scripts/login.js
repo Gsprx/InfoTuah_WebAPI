3df12d8507118f1ef43f3141e36fedf238db0ad1
Phlorion <gigadroll44thebest@gmail.com>
2025-01-01 19:59:40 +0200

    creating and storing uuid for new logins

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -5,8 +5,50 @@ loginBtn.addEventListener("click", (e) => {
   const username = document.getElementById("l_username");
   const password = document.getElementById("l_password");
 
-  const data = {
-    username: username,
-    password: password,
+  const userData = {
+    username: username.value,
+    password: password.value,
   };
+
+  login(userData);
 });
+
+const login = async (userData) => {
+  try {
+    const response = await fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+
+    if (!response.ok && response.status != 401) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data.success) {
+      console.log("Login successful:", data.message);
+      console.log("UUID:", data.sessionId);
+      // store user, session ID to session storage
+      sessionStorage.setItem(
+        "user",
+        JSON.stringify({
+          username: userData.username,
+          sessionId: data.sessionId,
+        })
+      );
+      window.location.href = "/";
+    } else {
+      console.error("Login failed:", data.message);
+      const errorMessage = document.querySelector(".error-message");
+      errorMessage.innerHTML = data.message;
+      errorMessage.style.color = "red";
+      errorMessage.style.fontSize = "1em";
+    }
+  } catch (error) {
+    console.error("An error occurred:", error.message);
+  }
+};
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 // Node.js Server script
-const uuid = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const express = require("express");
 
 // include DAO file
 const UserDAO = require("./public/scripts/DAO.js");
-const { PassThrough } = require("stream");
 
 const app = express();
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, "public")));
+// Middleware
+app.use(cors()); // Allow cross-origin requests
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files from the "public" directory
 
 // Serve index.html for the root route
 app.get("/", (req, res) => {
@@ -28,6 +31,29 @@ const usersList = [
 
 usersList.forEach((user) => {
   userDAO.addUser(user);
+});
+
+// login endpoint
+app.post("/login", (req, res) => {
+  // get the data
+  const data = req.body;
+
+  // try to authorize user
+  const userAuth = userDAO.validateUser(data.username, data.password);
+
+  // if user authorized create a uuid
+  if (userAuth) {
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      sessionId: uuidv4(),
+    });
+  } else {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid username or password",
+    });
+  }
 });
 
 // server
